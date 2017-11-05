7e37a6694ac676955e59662f28ac44137b378177
ecitlm <ecitlm@163.com>
2017-11-05 19:14:51 +0800

    ad

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
  * WeChat API 模块
  * @type {Object}
  * 用于将微信官方`API`封装为`Promise`方式
- * 小程序支持以`CommonJS`规范组织代码结构
  */
 const wechat = require('./utils/wechat.js')
 const api = require('./utils/api.js')
